7058a0947f15ec4adc778a591517351c177fe0a1
Experiencer <60224515+FireOnACandle@users.noreply.github.com>
2023-12-06 00:30:40 +0100

    Update and rename MathSolving.java to mathsolving.java

---
+++ mathsolving.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class MathSolving{
+  public static void main(String []args){
+    int[] legalNumbers = {1,3,4,6};
+    char[] operators = {"","(",")","+","-","/","*"};
+    
+    
+  }
+}
