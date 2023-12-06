2d76c7a9fdd2e12e3303b397d45461bff1a7ef12
Experiencer <60224515+FireOnACandle@users.noreply.github.com>
2023-12-06 09:06:27 +0100

    Update mathsolving.java

--- mathsolving.java
+++ mathsolving.java
@@ -1,5 +1,12 @@
 import java.util.Random;
 
+/* Use each of the numbers 1, 3, 4, and 6 exactly once with any
+of the four basic math operations (addition, subtraction,
+multiplication, and division) to total 24. Each number must be
+used once and only once, and you may define the order of
+operations; for example, 3 * (4 + 6) + 1 = 31 is valid, however
+incorrect, since it doesn’t total 24. */
+
 public class MathSolving{
   public static void main(String []args){
     int[] legalNumbers = {1,3,4,6};
